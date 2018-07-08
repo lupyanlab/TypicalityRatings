6f340e33149b04fe263f6c45a950a0da56df1ff2
Kevin Mui <kmui2@wisc.edu>
2018-07-08 00:44:28 -0500

    Done the TypicalityRatings experiment implementation

--- dev/experiment.js
+++ dev/experiment.js
@@ -18,7 +18,7 @@ function runExperiment(trials, subjCode, workerId, assignmentId, hitId) {
   let welcome_block = {
     type: "text",
     cont_key: " ",
-    text: `<h1>Image Simularity</h1>
+    text: `<h1>TypicalityRatings</h1>
         <p>Welcome to the experiment. Thank you for participating! Press SPACE to begin.</p>`
   };
 
@@ -32,10 +32,10 @@ function runExperiment(trials, subjCode, workerId, assignmentId, hitId) {
     key_forward: " ",
     key_backward: 8,
     pages: [
-      `<p>In this experiment, you will see two drawings and rate their simularity from 1 to 7.
+      `<p>In this experiment, you will see two drawings and rate their typicality from 1 to 5.
             </p> ${continue_space}`,
 
-      `<p>Use the 1-7 number keys to select your choice.
+      `<p>Use the 1-5 number keys to select your choice.
             </p> ${continue_space}`
     ]
   };
@@ -47,8 +47,8 @@ function runExperiment(trials, subjCode, workerId, assignmentId, hitId) {
   let num_trials = trials.length;
 
   _.forEach(trials, trial => {
-    images.push("stims/" + trial.pic1 + ".jpg");
-    images.push("stims/" + trial.pic2 + ".jpg");
+    images.push("images/" + trial.pic1 + ".png");
+    images.push("images/" + trial.pic2 + ".png");
 
     // Empty Response Data to be sent to be collected
     let response = {
@@ -56,15 +56,34 @@ function runExperiment(trials, subjCode, workerId, assignmentId, hitId) {
       workerId: workerId,
       assignmentId: assignmentId,
       hitId: hitId,
+      seed: trial.seed,
+      Category: trial.Category,
+      Subcategory1: trial.Subcategory1,
+      Subcategory2: trial.Subcategory2,
+      firstStimPosition: trial.firstStimPosition,
+      secondStimPosition: trial.secondStimPosition,
       pic1: trial.pic1,
       pic2: trial.pic2,
-      country1: trial.country1 || "unspecified",
-      country2: trial.country2 || "unspecified",
       expTimer: -1,
       response: -1,
+      choice: 'error: no choice selected',
       trial_number: trial_number,
       rt: -1
     };
+
+    let leftPic;
+    let rightPic;
+    if (trial.firstStimPosition === 'left') {
+      leftPic = trial.pic1;
+    } else if (trial.firstStimPosition === 'right') {
+      rightPic = trial.pic1;
+    }
+
+    if (trial.secondStimPosition === 'left') {
+      leftPic = trial.pic2;
+    } else if (trial.secondStimPosition === 'right') {
+      rightPic = trial.pic2;
+    }
 
     let stimulus = `
         <canvas width="800px" height="25px" id="bar"></canvas>
@@ -72,43 +91,41 @@ function runExperiment(trials, subjCode, workerId, assignmentId, hitId) {
             var barCanvas = document.getElementById('bar');
             var barCtx = barCanvas.getContext('2d');
             barCtx.roundRect(0, 0, barCanvas.width, barCanvas.height, 20).stroke();
-            barCtx.roundRect(0, 0, barCanvas.width*${trial_number /
-              num_trials}, barCanvas.height, 20).fill();
+            barCtx.roundRect(0, 0, barCanvas.width*${trial_number / num_trials}, barCanvas.height, 20).fill();
         </script>
         <h5 style="text-align:center;">Trial ${trial_number} of ${num_trials}</h5>
         <div style="width:100%;">
             <div style="width:50%;text-align:center;float:left;">
-                <img src="stims/${trial.pic1}.jpg" alt="${
-      trial.pic1
-    }" height="200px" align="middle" style="max-width:400px;"/> 
+                <img src="images/${leftPic}.png" alt="${leftPic}" height="200px" align="middle" style="max-width:400px;"/> 
             </div>
             <div style="width:50%;text-align:center;float:left;">
-                <img src="stims/${trial.pic2}.jpg" alt="${
-      trial.pic2
-    }" height="200px" align="middle" style="max-width:400px;width=50%;" />
+                <img src="images/${rightPic}.png" alt="${rightPic}" height="200px" align="middle" style="max-width:400px;width=50%;" />
             </div>
         </div>
         `;
 
-    const choices = ["1", "2", "3", "4", "5"];
+    const choices = [
+      "Left image much more typical", 
+      "Left image slightly more typical", 
+      "Both images equally typical", 
+      "Right image slightly more typical", 
+      "Right image more typical",
+    ];
+    
     let circlesSvgs = choices.map(choice => {
       return `
-            <svg class="choice" viewBox="0 0 100 100">
-                <circle class="empty-circle"/>
-                <text>${choice}</text>
-            </svg>
+        <div class="choice">
+          <svg viewBox="0 0 100 100">
+              <circle class="empty-circle"/>
+          </svg>
+          <div>${choice}</div>
+        </div>
         `;
     });
 
     let prompt = `
         <div class="bar">
-            <h2>
-                Almost Identical
-            </h2>
             ${circlesSvgs.join("")}
-            <h2>
-                Completely Different
-            </h2>
         </div>
     `;
 
@@ -116,7 +133,9 @@ function runExperiment(trials, subjCode, workerId, assignmentId, hitId) {
     let pictureTrial = {
       type: "single-stim",
       is_html: true,
-      choices,
+      choices: choices.map((choice, index) => {
+        return `${index + 1}`;
+      }),
 
       stimulus: stimulus,
 
@@ -126,12 +145,13 @@ function runExperiment(trials, subjCode, workerId, assignmentId, hitId) {
 
       on_finish: function(data) {
         response.response = String.fromCharCode(data.key_press);
+        response.choice = choices[Number(response.response)-1];
         response.rt = data.rt;
         response.expTimer = data.time_elapsed / 1000;
 
         // POST response data to server
         $.ajax({
-          url: "http://" + document.domain + ":" + PORT + "/trials",
+          url: "http://" + document.domain + ":" + PORT + "/data",
           type: "POST",
           contentType: "application/json",
           data: JSON.stringify(response),
@@ -154,32 +174,29 @@ function runExperiment(trials, subjCode, workerId, assignmentId, hitId) {
 
       prompt: function() {
         const circlesSvgs = choices.map((choice, index) => {
-          console.log(response.response);
-          if (index + 1 == response.response) {
+          if (choice == response.choice) {
             return `
-                    <svg class="choice" viewBox="0 0 100 100">
+                  <div class="choice">
+                    <svg viewBox="0 0 100 100">
                         <circle class="filled-circle"/>
-                        <text>${choice}</text>
                     </svg>
+                    <div>${choice}</div>
+                  </div>
                 `;
           }
           return `
-                <svg class="choice" viewBox="0 0 100 100">
-                    <circle class="empty-circle"/>
-                    <text>${choice}</text>
-                </svg>
+            <div class="choice">
+              <svg viewBox="0 0 100 100">
+                  <circle class="empty-circle"/>
+              </svg>
+              <div>${choice}</div>
+            </div>
             `;
         });
 
         const prompt = `
             <div class="bar">
-                <h2>
-                    Almost Identical
-                </h2>
                 ${circlesSvgs.join("")}
-                <h2>
-                    Completely Different
-                </h2>
             </div>
         `;
         return prompt;
@@ -193,13 +210,24 @@ function runExperiment(trials, subjCode, workerId, assignmentId, hitId) {
   let endmessage = `Thank you for participating! Your completion code is ${participantID}. Copy and paste this in 
         MTurk to get paid. If you have any questions or comments, please email jsulik@wisc.edu.`;
 
-  // add scale pic paths to images that need to be loaded
-  images.push("img/scale.png");
-  for (let i = 1; i <= 7; i++) images.push("img/scale" + i + ".jpg");
+    
+  Promise.all(images.map((image, index) => {
+    return loadImage(image)
+    .catch((error) => {
+      console.warn("Removing trial with image, " + image);
 
-  jsPsych.pluginAPI.preloadImages(images, function() {
+      // there are twice the number of images than trials
+      // we set trial to null so we can cleanly remove
+      // it later
+      trials[index / 2] = null;
+    });
+  }))
+  .then((images) => {
+    trials = trials.filter((trial, index) => {
+      return trial !== null;
+    });
     startExperiment();
-  });
+  })
 
   function startExperiment() {
     jsPsych.init({
